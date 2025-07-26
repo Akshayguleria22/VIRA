d2abdc33b5eb232336edb376c73df57c07fab756
Akshayguleria22 <akshayguleria700@gmail.com>
2025-07-26 19:22:53 +0530

    Delete Backend directory

--- Backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getAllUsers, getUser } from '../controllers/user.controller.js'
-import {authorize} from '../middlewares/auth.middlewares.js'
-
-const userRouter=new Router()
-
-userRouter.get('/', getAllUsers)
-userRouter.get('/:id', authorize, getUser)
-export default userRouter
